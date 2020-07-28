f70bc2151085a10bcb5ff0f1c39dace8dc82ff50
Gilmario Batista <gilmariosoftware@gmail.com>
2020-07-28 14:07:17 -0300

    OK

--- projects/generic-crud-lib/src/public-api.ts
+++ projects/generic-crud-lib/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of generic-crud-lib
  */
-
 export * from './lib/generic-crud-lib.service';
 export * from './lib/generic-crud-lib.component';
 export * from './lib/generic-crud-lib.module';
